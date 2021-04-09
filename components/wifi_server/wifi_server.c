cd7198abdca576e1df5d22eaf531be4af8c1e518
lorsi96 <lorsi@itba.edu.ar>
2021-04-08 21:15:17 -0300

    Sockets pseudo working

--- components/wifi_server/wifi_server.c
+++ components/wifi_server/wifi_server.c
@@ -1,7 +1,103 @@
 #include <stdio.h>
 #include "wifi_server.h"
 
-void func2(void)
-{
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
 
+
+#define HOST_IP_ADDR "192.168.0.14"
+#define PORT 3333
+
+static const char *TAG = "wifi_server";
+static IntConsumer_t onDataReceivedCallback;
+static uint32_t reconnect = 0;
+
+
+/** Internal Connection Constants. ****************/
+static const char host_ip[] = HOST_IP_ADDR;
+static const int addr_family = AF_INET;
+static const int ip_protocol = IPPROTO_IP;
+
+// static char tx_buffer[128];
+static char rx_buffer[128];
+static struct sockaddr_in dest_addr;
+
+static int sock;
+
+/** Flow Variables ********************************/
+static char dataToSend[] = "0";
+static bool isDataToSendReady = false;
+
+void static PDMNetwork_reinit() {
+    ESP_LOGE(TAG, "Shutting down socket and restarting...");
+    shutdown(sock, 0);
+    close(sock);
+    PDMNetwork_init(onDataReceivedCallback);
+}
+
+bool PDMNetwork_init(IntConsumer_t onDataReceived) {
+    dest_addr.sin_addr.s_addr = inet_addr(host_ip);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    onDataReceivedCallback = onDataReceived;
+    sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        return false;
+    }
+    ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
+
+    int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in6));
+
+    if (fcntl(sock, F_SETFL,  O_NONBLOCK) < 0) {
+        ESP_LOGE(TAG, "Failed to set nonblocking error");
+    }
+
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+        return false;
+    }
+    ESP_LOGI(TAG, "Successfully connected");
+    return true;
+}
+
+void PDMNetwork_send(const uint32_t message) {
+    dataToSend[0] =  message + 0x30;
+    isDataToSendReady = true;
+}
+
+void PDMNetwork_task() {
+    ESP_LOGI(TAG, "Task Running");
+
+    if (isDataToSendReady) {
+        int err = send(sock, dataToSend, strlen(dataToSend), 0);
+        ESP_LOGI(TAG, "Message sent");
+        if (err < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            PDMNetwork_reinit();
+        }
+        isDataToSendReady = false;
+    }
+
+    ESP_LOGI(TAG, "Attempting Reception");
+    int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, MSG_DONTWAIT);
+    ESP_LOGI(TAG, "After Reception %d", len);
+    if (len > 0) {
+        onDataReceivedCallback((uint32_t)(rx_buffer[0]-0x30));    
+        rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+        ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+        ESP_LOGI(TAG, "%s", rx_buffer);
+        // PDMNetwork_reinit();
+        return;
+    }
 }
--- main/lorsipdm.c
+++ main/lorsipdm.c
@@ -2,10 +2,53 @@
 #include <esp_system.h>
 #include "bluetooth_client.h"
 #include "wifi_server.h"
+#include "protocol_examples_common.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
 
+static void testConsumer(uint32_t data) {
+    switch(data) {
+    case 0:
+      ESP_LOGI("Main", "Speed request arrived");
+      PDMNetwork_send(0);
+      break;
+    case 1:
+      ESP_LOGI("Main", "Reset request arrived");
+      break;
+    }
+}
+
+static void init() {
+    while(!PDMNetwork_init(testConsumer)) {
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void loop() {
+    PDMNetwork_task();
+}
+
+void superLoopTask(void* pvParameters) {
+    init();
+    for(;;) {
+        loop();
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void)
 {
-    func();
-    func2();
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(superLoopTask, "lorsi_pdm", 4096, NULL, 5, NULL);
 }
